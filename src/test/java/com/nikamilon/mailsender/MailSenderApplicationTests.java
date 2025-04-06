2c7619d6bcce1ef1139f8f39c1d10346bd9aac14
Nikon <102325439+MrNikaMilon@users.noreply.github.com>
2025-04-06 16:56:46 +0300

    Initial commit

---
+++ src/test/java/com/nikamilon/mailsender/MailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikamilon.mailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
